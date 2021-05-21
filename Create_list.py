0e72cdd8cba70ec6ec921fa45a63a0d4d0e664a4
Sandipan Mukherjee <sandipan503@gmail.com>
2021-05-21 19:31:22 +0200

    commit

--- Create_list.py
+++ Create_list.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 
 
+
 def create_list(h5_loc):
     h5_file = h5.File(h5_loc,'r')
     position = h5_file['position']
@@ -24,9 +25,10 @@ def read_list(list_loc):
    df =  pd.read_csv(list_loc,header=None)
    return df
 
-index_list = create_list("/home/Mukherjee/Data/Cross_ext.h5")
+
+#index_list = create_list("/home/Mukherjee/Data/Cross_ext.h5")
 #save_list(index_list,list_loc="/home/Mukherjee/Data/Cross_ext_index.csv")
-df = read_list(list_loc="/home/Mukherjee/Data/Cross_ext_index.csv")
+#df = read_list(list_loc="/home/Mukherjee/Data/Cross_ext_index.csv")
 
 
 
